4ec71e4d4d5126e9b9fec16bf579e72219f05d56
Rohan Mendon <rohanmendon26@gmail.com>
2025-10-26 16:18:22 +0530

    updated

---
+++ 54.)Introduction to JavaScript & installing Node.js/new.js
@@ -0,0 +1,3 @@
+console.log("Code is running")
+console.log("Code is running fast")
+console.log("Code is running slow")
---
+++ 54.)Introduction to JavaScript & installing Node.js/script.js
@@ -0,0 +1,19 @@
+alert("Hello World")
+console.log("Code is running")
+console.log("Code is running fast")
+console.log("Code is running slow")
+
+var a =prompt("Enter your number")
+var is =confirm("Do you want to leave this site and vlast your computer?")
+
+if(is){
+    console.log("computer is blasting ")
+}
+
+else{
+    console.log("Now you are good")
+}
+
+document.title="Introduction to js"
+document.body.style.backgroundColor="green"
+console.log("your number is "+ a)
