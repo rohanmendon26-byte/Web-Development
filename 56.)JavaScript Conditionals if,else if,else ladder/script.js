db943822e35d32fcd0853ef7ce47a0b2060e28bb
Rohan Mendon <rohanmendon26@gmail.com>
2025-10-27 22:27:15 +0530

    updated

---
+++ 56.)JavaScript Conditionals if,else if,else ladder/script.js
@@ -0,0 +1,15 @@
+console.log("Hello i am Conditional tutorial ")
+
+let age=45;
+
+
+if(age>18){
+    console.log("You can drive");
+}
+
+else{
+    console.log("You cannot drive");
+}
+
+
+// === --> equal and type
