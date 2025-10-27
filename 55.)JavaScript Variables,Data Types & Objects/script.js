9b584f58630bb8f0051ac39bc52ba1f6bcbc7fed
Rohan Mendon <rohanmendon26@gmail.com>
2025-10-27 06:26:44 +0530

    updated

---
+++ 55.)JavaScript Variables,Data Types & Objects/script.js
@@ -0,0 +1,37 @@
+console.log("Hello bhai")
+
+var a=23;
+var b=34;
+var c="rohan";
+
+// console.log(a+b)
+// console.log(typeof a)
+// console.log(typeof c)
+
+{
+    let a=98;
+    console.log(a)
+}
+
+//var is globally scoped and let is locally scoped
+
+// const a1=98;
+// a1=87;   //you cannot change the value of a1 error is occured beacuse a1 is constant
+
+let x="Harry Bhai";
+let y=22;
+let z=3.55;
+const p=true;
+let q=undefined;
+let r=null;
+
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o={
+    "name":"Rohan",
+    "USN":"4MW24CS117"
+}
+
+o.salary="100 crores"
+console.log(o)
