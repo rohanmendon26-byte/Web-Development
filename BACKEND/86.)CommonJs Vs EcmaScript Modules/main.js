5adc1efd364e8462bb37dd6d44a3855bdc7ce78b
Rohan Mendon <rohanmendon26@gmail.com>
2026-02-28 21:14:28 +0530

    commonjs and ecmascript

--- BACKEND/86.)CommonJs Vs EcmaScript Modules/main.js
+++ BACKEND/86.)CommonJs Vs EcmaScript Modules/main.js
@@ -6,17 +6,31 @@
 
 //node main.js --> This command runs your Node.js file main.js using nodemon instead of normal node.
 
-const { createServer } = require('node:http');
+// const { createServer } = require('node:http');
+// const fs=require("fs")
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// import { createServer } from "http";
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
+// const hostname = "127.0.0.1";
+// const port = 3000;
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/html");
+//   res.end("<h1>Hello World</h1>");
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+// import {a,b,d} from "./mymodule.js"
+// console.log(a,b,d);
+
+
+import obj from "./mymodule.js"
+console.log(obj);
+
+// const a =require("./mymodule2.js")
+// console.log(a)
