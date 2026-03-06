0292b7476755290e2b9e9c4f515a088a08608e39
Rohan Mendon <rohanmendon26@gmail.com>
2026-03-06 21:05:10 +0530

    completed

--- BACKEND/89.)Response- Request and Routers in Express/main.js
+++ BACKEND/89.)Response- Request and Routers in Express/main.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const blog = require('./route/blog')
+const shop=require('./route/shop')
+
+
 const app = express()
 const port = 3000
+
 app.use(express.static('public'))
+app.use('/blog', blog)
+app.use('/shop', shop)
+
 
 
 app.get('/', (req, res) => {
@@ -13,10 +21,21 @@ app.post('/', (req, res) => {
   console.log("Hey it's a post request");
   res.send('Hello World! post')
 })
+
 app.put('/', (req, res) => {
   console.log("Hey it's a put request");
   res.send('Hello World! put')
 })
+
+app.get('/index', (req, res) => {
+  console.log("Hey it's index");
+  res.sendFile('template/index.html',{root:__dirname})
+})
+
+app.get('/api', (req, res) => {
+  res.json({a:1,b:2,c:3,d:4,name:["rohan","harry"]})
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
