5982d6778b9927d84f228b3bd48212efd7171f55
Rohan Mendon <rohanmendon26@gmail.com>
2026-03-11 20:53:59 +0530

    Merge remote changes

--- BACKEND/90.)Middlewares in Express Js/main.js
+++ BACKEND/90.)Middlewares in Express Js/main.js
@@ -2,12 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fs=require("fs")
+const blog = require('./routes/blog')
+
+
+
+app.use('/blog', blog)
 
 // app.use(express.static("public"))
 
 
 //Middleware 1 -Logger for our application
 app.use((req, res, next)=> {
+  console.log(req.headers);
+  req.rohan="I am Ronny bhai";
+  
   fs.appendFileSync("logs.txt",`${Date.now()} is a ${req.method}\n`)
   console.log(`${Date.now()} is a ${req.method}`)
   next()
@@ -26,7 +34,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-  res.send('Hello about')
+  res.send('Hello about '+req.rohan)
 })
 
 app.get('/contact', (req, res) => {
