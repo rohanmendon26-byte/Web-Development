67ea7a304682f3f6d6280ff05dbef25e772b4218
Rohan Mendon <rohanmendon26@gmail.com>
2026-05-26 06:08:40 +0530

    completed

--- BACKEND/Exercise 18 - Display the Cards/src/App.jsx
+++ BACKEND/Exercise 18 - Display the Cards/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -6,12 +6,31 @@ import './App.css'
 import Navbar from './Components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState([])
+
+  const fetchData = async () => {
+    let a = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let data=await a.json();
+    setCards(data)
+    console.log(data)
+}
+
+useEffect(() => {
+ fetchData()
+}, [])
+
 
   return (
     <>
-      <div>
-      <Navbar/>
+        <Navbar />
+      <div class="container">
+        {cards.map((card)=>{
+          return <div key={card.id} class="card">
+            <h1>{card.title}</h1>
+            <p>{card.body}</p>
+            <span>By:UserId:{card.userId} and </span>
+          </div>
+        })}
       </div>
     </>
   )
