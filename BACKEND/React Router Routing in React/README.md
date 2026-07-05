b61998c2937e92cef224a0e967cfef3fa1d2dd1e
Rohan Mendon <rohanmendon26@gmail.com>
2026-07-05 22:39:12 +0530

    completed

---
+++ BACKEND/React Router Routing in React/README.md
@@ -0,0 +1,6 @@
+npm i react-router-dom
+
+What it does
+Downloads the React Router DOM package from the npm registry.
+Adds it to your project's node_modules folder.
+Updates the package.json file by adding react-router-dom under the dependencies section.
---
+++ BACKEND/React Router Routing in React/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Login from './components/Login'
+import About from './components/About'
+import User from './components/User'
+import { createBrowserRouter,RouterProvider} from 'react-router-dom'
+
+function App() {
+  const router=createBrowserRouter([
+    {
+      path: "/",
+      element:<><Navbar/><Home/></>
+    },
+    {
+      path:"/login",
+      element:<><Navbar/><Login/></>
+    },
+    {
+      path:"/about",
+      element:<><Navbar/><About/></>
+    },
+    {
+      path:"/user/:username",
+      element:<><Navbar/><User/></>
+    }
+])
+
+  return (
+    <>
+    
+    <RouterProvider router={router}/>
+      
+    </>
+  )
+}
+
+export default App
