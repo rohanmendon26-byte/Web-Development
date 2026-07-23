14b7d05dca91457aea9342b9ce54723daf0332d6
Rohan Mendon <rohanmendon26@gmail.com>
2026-07-23 10:43:18 +0530

    completed

---
+++ BACKEND/Introduction to Next.js and File based Routing/first/app/about/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-xl">Problems Solved by Next.js</h1>
+      <ul>
+        <li>Full stack Solution</li>
+        <li>File Based Routing</li>
+        <li>Additional feautures like router from next/navigation</li>
+        <li>Optimized rendering </li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
